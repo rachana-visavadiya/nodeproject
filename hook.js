daaeb69c47dad3e6a16b7ec5a5f94b776ed2d24a
ngengesenior <seniorngenge@gmail.com>
2023-03-17 00:43:27 +0100

    Update npm

--- hook.js
+++ hook.js
@@ -15,26 +15,30 @@ class Script {
     process_incoming_request({ request }) {
         
         // console is a global helper to improve debug
-        console.log("------------------------------------------------");
+
         let reqContent = request.content;
-        console.log(JSON.stringify(reqContent));
         let message = "";
         const name = reqContent.commitAuthor;
         const authorEmail = reqContent.commitAuthorEmail;
         const commitDate = reqContent.commitDate;
         const commitMessage = reqContent.commitMessage;
         const repo = reqContent.repositoryName;
+        const commitId = reqContent.commitId;
         const projectName = reqContent.projectName;
+        const buildNumber = reqContent.buildNumber;
+        const buildId = reqContent.buildId;
         message += "Commit by author:" + name + " whose email is " + authorEmail;
         message += " on the repo " + "[" + projectName + "](https://github.com/" + repo + ")";
-        message += " on " + commitDate + " and their commit message was,'" + commitMessage + "'"
+        message += " on " + commitDate + " and their commit message was,'" + commitMessage + "'\n"
+        message += "Build details, build ID:" + buildId + ", build number:" + buildNumber + "\n"
+        message += "You can check the [Commit]((https://github.com/" + repo + "/commit/" + commitId + ")"
 
         const content = {
             "emoji": ":smiley:",
             "text": message,
             "attachments": [
                 {
-                    "title": "Appveyor",
+                    "title": projectName + " build",
                     "title_link": "https://rocket.chat",
                     "text": "Appveyor is a CI tool",
                     "image_url": "https://writing-demo.dev.rocket.chat/images/integration-attachment-example.png",
